420c9c4214e6d6681c4a44df3fbc7ff183ddd325
Nico28504 <jorge.acuna2520@alumnos.udg.mx>
2025-08-20 17:57:41 -0600

    Add files via upload

---
+++ _and.v
@@ -0,0 +1,8 @@
+//1.Definicion del modulo y sus Ins and Outs
+//Dentro del parentesis se define los I/O
+module _and (input a, input b, output c);
+//2.Definen cables o componentes 
+//NA
+//3.Asignaciones, instancias y conexiones
+assign c = a & b;
+endmodule
